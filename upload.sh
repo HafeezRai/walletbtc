a96dfa0c4a1b1e2fbbffbda393017fee5bcb0f71
luis.martinez <luis.martinez@abstracta.com.uy>
2017-11-28 16:37:53 -0300

    start saucelabs

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 export ANDROID_SAUCE_STORAGE_DEBUG="https://saucelabs.com/rest/v1/storage/$SAUCE_USERNAME/edge.app?overwrite=true"
-export ANDROID_BUILD_APP_DEBUG=app/build/outputs/apk/app-debug.apk
+export ANDROID_BUILD_APP_DEBUG=$HOME/app-debug.apk
 
 curl -u $SAUCE_USERNAME:$SAUCE_ACCESS_KEY -X POST -H 'Content-Type: application/octet-stream' $ANDROID_SAUCE_STORAGE_DEBUG --data-binary @$ANDROID_BUILD_APP_DEBUG
 
