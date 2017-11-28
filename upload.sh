c665986a9a99c6ae88966fcbc953e70f3c0dd8ed
luis.martinez <luis.martinez@abstracta.com.uy>
2017-11-28 17:16:25 -0300

    start saucelabs

--- upload.sh
+++ upload.sh
@@ -3,4 +3,6 @@ export ANDROID_BUILD_APP_DEBUG=$HOME/app-debug.apk
 
 curl -u $SAUCE_USERNAME:$SAUCE_ACCESS_KEY -X POST -H 'Content-Type: application/octet-stream' $ANDROID_SAUCE_STORAGE_DEBUG --data-binary @$ANDROID_BUILD_APP_DEBUG
 
-tests/wdio local.conf.js
+cd tests
+
+local.conf.js
