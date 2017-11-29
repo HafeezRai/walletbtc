e1a4d2305943b41da7014296af8fec7cc16ede06
luis.martinez <luis.martinez@abstracta.com.uy>
2017-11-29 12:10:19 -0300

    start saucelabs

--- upload.sh
+++ upload.sh
@@ -3,13 +3,11 @@ export ANDROID_BUILD_APP_DEBUG=$HOME/app-debug.apk
 
 curl -u $SAUCE_USERNAME:$SAUCE_ACCESS_KEY -X POST -H 'Content-Type: application/octet-stream' $ANDROID_SAUCE_STORAGE_DEBUG --data-binary @$ANDROID_BUILD_APP_DEBUG
 
-yarn add --peer
-
-npm install
-
 ls
 
 cd "./tests"
+
+npm install
 
 ls
 
